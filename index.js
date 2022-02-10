696dfdd067597d9ae417e78366324805536b57f5
natalie <nataliemae263@gmail.com>
2022-02-09 19:01:47 -0600

    HTML

---
+++ index.js
@@ -0,0 +1,16 @@
+const map = L.map('map', {
+    center: [-29.50],
+    zoom: 3.5
+});
+
+const changeText = () 
+
+//user interface 
+//list of search items: coffee, restaurants, hotels, markets(grocery stores)
+//add a spot on the map of my location
+//add 5 locations for each topic above near my location for possible
+//options on where to go. 
+//create a drop down menu
+//create a submit button
+//do all of this in JS??? Or an I add some to HTML depending on how I 
+//understand it? 
